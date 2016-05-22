07ec5f554f738993605ae01d2f38ff351320508f
Upscale Software Team <info@upscalesoftware.com>
2016-05-21 21:53:56 -0700

    Entry point refactoring
    - Delegated server object composition to respective factory
    - Reduced amount of code in the entry point

--- server.php
+++ server.php
@@ -3,7 +3,6 @@
 namespace Upscale\HttpServerMock;
 
 use Zend\Diactoros\Response;
-use Zend\Diactoros\Server;
 use Zend\Diactoros\ServerRequestFactory;
 
 try {
@@ -16,44 +15,20 @@ try {
 
     $configFileChoices = $configFile ? [$configFile] : [__DIR__ . '/config.json', __DIR__ . '/config.json.dist'];
 
-    $configFallback = new Config\Fallback();
-    $configStream = $configFallback->getFirstReadableStream($configFileChoices);
-    $configSource = new Config\Source\Stream($configStream);
-
-    $placeholders = [
+    $configPlaceholders = [
         '%base_dir%' => __DIR__,
         '%document_root%' => ServerRequestFactory::get('DOCUMENT_ROOT', $request->getServerParams()),
         '%base_url_path%' => dirname(ServerRequestFactory::get('SCRIPT_NAME', $request->getServerParams())),
     ];
-    $configSource = new Config\Source\Decorator\SubstringSubstitution($configSource, $placeholders);
-
-    $streamFactory = new StreamFactory();
-    $requestFactory = new RequestFactory($request, $streamFactory);
-    $responseFactory = new ResponseFactory($streamFactory);
-
-    $configParserFactory = new Config\Syntax\ParserFactory();
-    $configParser = $configParserFactory->create($configType);
-    $configRuleFactory = new Config\RuleFactory();
-
-    $config = new Config($configSource, $configParser, $requestFactory, $responseFactory, $configRuleFactory);
-
-    $formatterRegistry = new Body\FormatterRegistry([
-        'binary'    => new Body\Formatter\Binary(),
-        'html'      => new Body\Formatter\Html(),
-        'json'      => new Body\Formatter\Json(),
-        'text'      => new Body\Formatter\Text(),
-        'xml'       => new Body\Formatter\Xml(),
-    ]);
-
-    $comparatorManager = new Request\ComparatorManager($formatterRegistry);
 
     $formatDetector = new Request\FormatDetector(require __DIR__ . '/mime-types.php', 'binary');
 
-    $app = new App($config, $formatDetector, $comparatorManager);
+    $defaultResponse = new Response('php://memory', 400, ['Content-Type' => 'text/plain']);
 
-    $responseNotFound = new Response('php://memory', 400, ['Content-Type' => 'text/plain']);
-
-    $server = new Server([$app, 'handle'], $request, $responseNotFound);
+    $serverFactory = new ServerFactory();
+    $server = $serverFactory->create(
+        $request, $formatDetector, $defaultResponse, $configFileChoices, $configType, $configPlaceholders
+    );
     $server->listen();
 
 } catch (\Exception $e) {
---
+++ src/ServerFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Upscale\HttpServerMock;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Server;
+
+class ServerFactory
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param Request\FormatDetector $formatDetector
+     * @param ResponseInterface $defaultResponse
+     * @param array $configFileChoices
+     * @param string $configType
+     * @param array $configPlaceholders
+     * @return Server
+     */
+    public function create(
+        ServerRequestInterface $request,
+        Request\FormatDetector $formatDetector,
+        ResponseInterface $defaultResponse,
+        array $configFileChoices,
+        $configType,
+        array $configPlaceholders = []
+    ) {
+        $configFallback = new Config\Fallback();
+        $configStream = $configFallback->getFirstReadableStream($configFileChoices);
+        $configSource = new Config\Source\Stream($configStream);
+        $configSource = new Config\Source\Decorator\SubstringSubstitution($configSource, $configPlaceholders);
+
+        $streamFactory = new StreamFactory();
+        $requestFactory = new RequestFactory($request, $streamFactory);
+        $responseFactory = new ResponseFactory($streamFactory);
+
+        $configParserFactory = new Config\Syntax\ParserFactory();
+        $configParser = $configParserFactory->create($configType);
+        $configRuleFactory = new Config\RuleFactory();
+
+        $config = new Config($configSource, $configParser, $requestFactory, $responseFactory, $configRuleFactory);
+
+        $formatterRegistry = new Body\FormatterRegistry([
+            'binary'    => new Body\Formatter\Binary(),
+            'html'      => new Body\Formatter\Html(),
+            'json'      => new Body\Formatter\Json(),
+            'text'      => new Body\Formatter\Text(),
+            'xml'       => new Body\Formatter\Xml(),
+        ]);
+
+        $comparatorManager = new Request\ComparatorManager($formatterRegistry);
+
+        $app = new App($config, $formatDetector, $comparatorManager);
+
+        $server = new Server([$app, 'handle'], $request, $defaultResponse);
+
+        return $server;
+    }
+}
